1fe0294233da29ffeb9db31474545465c7eb4546
KI MIN SOO <banna111222@gmail.com>
2020-09-02 16:31:27 +0900

    기본적인 기능을 가진 메모장 완성

--- AndroidStudioProjects/Memo/app/src/main/java/com/example/memo/MainActivity.java
+++ AndroidStudioProjects/Memo/app/src/main/java/com/example/memo/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.memo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText memoEdit = null;
+    TextFileManager tfm = new TextFileManager(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        memoEdit = (EditText) findViewById(R.id.editTextTextMultiLine); //memoEdit 이라는 녀석은 edittextmultiline이라는 아이디를 가진 화면에 텍스트를 보여주는 아이다.
     }
+
+    public void onClick(View v){        //아마도 뷰 안에 있는 어떤 것이 눌렸을 때! 를 onClick이라는 메소드를 통해 표현한다.
+        switch(v.getId()){
+            //저장
+            case R.id.saveBtn : {
+                String memoData = memoEdit.getText().toString();
+                if(memoData.equals("")) {       //아무것도 안 적었는데 저장하려고 한다면, 멈춘다.
+                    Toast.makeText(this, "내용을 입력해주세요.", Toast.LENGTH_LONG).show();
+                    break;
+                }
+                tfm.save(memoData);
+                memoEdit.setText("");
+                Toast.makeText(this, "저장 완료", Toast.LENGTH_LONG).show();
+                break;
+            }
+            //불러오기
+            case R.id.loadBtn : {
+                String memoData = tfm.load();
+                memoEdit.setText(memoData);
+
+                Toast.makeText(this, "불러오기 완료", Toast.LENGTH_LONG).show();
+                break;
+            }
+            //지우기
+            case R.id.deleteBtn : {
+                tfm.delete();
+                memoEdit.setText("");
+
+                Toast.makeText(this, "삭제 완료", Toast.LENGTH_LONG).show();
+                break;
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
 }
---
+++ AndroidStudioProjects/Memo/app/src/main/java/com/example/memo/TextFileManager.java
@@ -0,0 +1,52 @@
+package com.example.memo;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TextFileManager {
+    private static final String FILE_NAME="memo.txt"; //메모 내용을 저장할 파일 이름
+    Context mContext = null;
+
+    public TextFileManager(Context context){
+        mContext = context;
+    }
+
+    //메모 저장
+    public void save(String strData){
+        if(strData==null||strData.equals("")){
+            return;
+        }
+        FileOutputStream fosMemo = null;
+        try{
+            fosMemo=mContext.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            fosMemo.write(strData.getBytes());
+            fosMemo.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //메모 불러오기
+    public String load(){
+        try{
+            FileInputStream fisMemo = mContext.openFileInput(FILE_NAME);
+            byte[] memoData = new byte[fisMemo.available()];
+            while(fisMemo.read(memoData)!=-1){}
+            return new String(memoData);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    //메모 지우기
+    public void delete(){
+        mContext.deleteFile(FILE_NAME);
+    }
+
+}
